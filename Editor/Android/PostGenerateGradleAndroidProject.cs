fc4522470c1d9ba54f5ee85fbfaae55ac8ce0928
Unity Technologies <@unity.com>
2019-04-03 11:46:57 +0000

    com.unity.analytics@3.1.1 ## [3.1.1] - 2018/08/21 - Add DataPrivacy plugin into package. - Fixed an issue where Android project build would fail when proguard is enabled   in publishing settings. - Fixed an issue where iOS product archive would fail because bitcode was not   enabled.

--- Editor/Android/PostGenerateGradleAndroidProject.cs
+++ Editor/Android/PostGenerateGradleAndroidProject.cs
@@ -3,14 +3,14 @@ using UnityEditor.Android;
 
 namespace UnityEditor.Analytics
 {
-	public class PostGenerateGradleAndroidProject : IPostGenerateGradleAndroidProject
-	{
-		public int callbackOrder { get { return 0; } }
+    public class PostGenerateGradleAndroidProject : IPostGenerateGradleAndroidProject
+    {
+        public int callbackOrder { get { return 0; } }
 
-		public void OnPostGenerateGradleAndroidProject(string path)
-		{
-			PatchAndroidManifest.PatchManifest(path);
-		}
-	}
+        public void OnPostGenerateGradleAndroidProject(string path)
+        {
+            PatchAndroidManifest.PatchManifest(path);
+        }
+    }
 }
 #endif
